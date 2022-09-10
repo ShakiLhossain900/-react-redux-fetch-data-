55055797a2009bcad546c726d6fa3f1797540af3
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-09-10 19:38:48 +0600

    adding plan

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 function App() {
   return <div className="App">
-    <h1>react redux fetch data</h1> ``
+    <h1>react redux fetch data</h1> 
   </div>;
 }
 
 export default App;
+
+
+
+//react-redux | fetch data plan for this project 
+// 1. install packages
+//2. constants define
+//3. async action creator 
+// 4. reducer 
+// 5. create store 
+// 6. provide store
+//  7. use store
+// 8. adding store
