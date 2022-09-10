09312f48823994cf9386751768ea6fa5837a9ae4
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-09-10 22:41:51 +0600

    adding all think in todos.js

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,11 +1,18 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getAllTodos } from "../services/actions/todosActions";
 
 const Todos = () => {
-  const todos = useSelector((state) => console.log(state));
+  const { isLoading, todos, error } = useSelector((state) => state);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getAllTodos());
+  }, []);
   return (
     <div>
       <h1> Todos app</h1>
+      {isLoading && <h3>Loading ...</h3>}
+      {error && <h3>{error.message}</h3>}
     </div>
   );
 };
